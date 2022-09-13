3def9bcb1b9aa6ce847934996e361ba42aed8e5b
Arittra101 <avishek51775@gmail.com>
2022-09-13 17:57:01 +0600

    updated

---
+++ CodeForce/Ok Try and catch/A. Two Elevators.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+lli i, j;
+
+#define pii pair<lli, lli>
+#define vi vector<lli>
+#define pb push_back
+#define mp make_pair
+#define yes cout << "YES" \
+                 << "\n";
+#define no cout << "NO" \
+                << "\n";
+#define SORT(v) sort(v.begin(), v.end());
+#define R_SORT(v) sort(v.begin(), v.end(), greater<lli>());
+#define en "\n"
+lli s;
+
+bool SORT1(pair<lli, lli> p1, pair<lli, lli> p2)
+{
+    return p1.first < p2.first;
+}
+void solve()
+{
+
+    lli a, b, c;
+    cin >> a >> b >> c;
+
+    a = abs(a - 1);
+
+    b = abs(c - b) + abs(c - 1);
+
+    if (a < b)
+        cout << 1 << endl;
+    else if (a > b)
+        cout << 2 << endl;
+    else
+        cout << 3 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    lli t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    // cout<<"Case "<<l<<": ";
+}
---
+++ CodeForce/Ok Try and catch/B. Decode String.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+lli i, j;
+ 
+#define pii pair<lli, lli>
+#define vi vector<lli>
+#define pb push_back
+#define mp make_pair
+#define yes cout << "YES" \
+                 << "\n";
+#define no cout << "NO" \
+                << "\n";
+#define SORT(v) sort(v.begin(), v.end());
+#define R_SORT(v) sort(v.begin(), v.end(), greater<lli>());
+#define en "\n"
+lli s;
+ 
+bool SORT1(pair<lli, lli> p1, pair<lli, lli> p2)
+{
+    return p1.first < p2.first;
+}
+void solve()
+{
+ 
+     lli n;
+     cin>>n;
+     string s;
+     cin>>s;
+    stack<char>st;
+
+    for(i=n-1;i>=0;)
+    {
+        if(s[i]=='0')
+        {
+         
+            lli x  =  (s[i-2]-'0')*10 + (s[i-1]-'0') ;
+             
+             st.push(char(96+x));
+           // cout<<x<<" ";
+            i-=3;
+        }
+        else {
+         
+            lli x  =  (s[i]-'0') ;
+           
+             st.push(char(96+x));
+             i--;
+
+        }
+
+
+    }
+    while(st.size()!=0)
+    {
+        cout<<st.top();
+        st.pop();
+    }
+    cout<<endl;
+   
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    lli t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    // cout<<"Case "<<l<<": ";
+}
